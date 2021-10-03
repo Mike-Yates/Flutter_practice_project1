647ce864f66f6e8de9eeaeb56560a6f46a3e64ac
Mike-Yates <mjy5xy@virginia.edu>
2021-10-03 01:50:36 -0400

    completed part 6 and 7 of class, Playing with scaffolding widget options, and customizing the text with different fonts and sizes.

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,28 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MaterialApp(
-  home: Text("ok!"),
+void main() => runApp(MaterialApp( // runApp needs to be passed a root widget
+  home: Scaffold(
+    appBar: AppBar(
+      title: Text('Mikes title'),
+      centerTitle: true,
+      backgroundColor: Colors.green,
+    ),
+    body: Center( // all widgets must have a property. when youre nesting widgets inside of each other, use child:
+      child: Text('Hello!', style: TextStyle(
+        fontSize: 30.0,
+        fontWeight: FontWeight.bold,
+        letterSpacing: 2.0,
+        fontFamily: 'IndieFlower',
+        color: Colors.red,
+      ),
+      ),
 
-));  // runApp needs to be passed a root widget
+    ),
+    floatingActionButton: FloatingActionButton(
+      child: Text('Click'),
+      onPressed: () { },  // onPressed is an anonymous function
+      backgroundColor: Colors.green,
+    ),
+  ),
+
+));
