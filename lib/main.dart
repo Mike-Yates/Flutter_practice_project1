47c1c4d380e7d617f001f962a2d55e39bfcb2049
Mike-Yates <mjy5xy@virginia.edu>
2021-11-08 16:52:40 -0500

    Added an icon. experimenting with the ICON class

--- lib/main.dart
+++ lib/main.dart
@@ -16,10 +16,11 @@ class Home extends StatelessWidget {
           backgroundColor: Colors.green,
         ),
         body: Center( // all widgets must have a property. when youre nesting widgets inside of each other, use child:
-          child: Image.asset('assets/Cristiano_Ronaldo_Picture.jpeg'
-            // image: AssetImage('assets/Cristiano_Ronaldo_Picture.jpeg'),
-            // image: NetworkImage('https://upload.wikimedia.org/wikipedia/commons/b/b2/Cristiano_Ronaldo_%28163461623%29.jpeg'),
-          ),
+          child: Icon(
+            Icons.account_balance_sharp,
+            color: Colors.black,
+            size: 50.0,
+          )
         ),
         floatingActionButton: FloatingActionButton(
           onPressed: () { },  // onPressed is an anonymous function
