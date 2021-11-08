0a19f4c07455cc789379ee7a9b1da88d08f03317
Mike-Yates <mjy5xy@virginia.edu>
2021-11-08 15:49:01 -0500

    created my own widget that extends the stateless widget template

--- lib/main.dart
+++ lib/main.dart
@@ -1,28 +1,37 @@
 import 'package:flutter/material.dart';
 
 void main() => runApp(MaterialApp( // runApp needs to be passed a root widget
-  home: Scaffold(
-    appBar: AppBar(
-      title: Text('Mikes title'),
-      centerTitle: true,
-      backgroundColor: Colors.green,
-    ),
-    body: Center( // all widgets must have a property. when youre nesting widgets inside of each other, use child:
-      child: Text('Hello!', style: TextStyle(
-        fontSize: 30.0,
-        fontWeight: FontWeight.bold,
-        letterSpacing: 2.0,
-        fontFamily: 'IndieFlower',
-        color: Colors.red,
-      ),
-      ),
-
-    ),
-    floatingActionButton: FloatingActionButton(
-      child: Text('Click'),
-      onPressed: () { },  // onPressed is an anonymous function
-      backgroundColor: Colors.green,
-    ),
-  ),
-
+  home: Home(),
 ));
+
+
+class Home extends StatelessWidget {
+  // final String title;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text('Mikes title!!'),
+          centerTitle: true,
+          backgroundColor: Colors.green,
+        ),
+        body: const Center( // all widgets must have a property. when youre nesting widgets inside of each other, use child:
+          child: Text(
+            'Hello!',
+            style: TextStyle(
+              fontSize: 30.0,
+              fontWeight: FontWeight.bold,
+              letterSpacing: 2.0,
+              fontFamily: 'IndieFlower',
+              color: Colors.red,
+            ),
+          ),
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () { },  // onPressed is an anonymous function
+          child: Text('click'),
+          backgroundColor: Colors.green,
+        ),
+      );
+  }
+}
